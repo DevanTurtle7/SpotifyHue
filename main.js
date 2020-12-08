de56409c00948894d72b0184ba4778514183bb4b
Devan <devankav7@gmail.com>
2020-12-08 00:14:09 -0500

    added async get function for token

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-
-
 async function getClientSecret() {
-    var docRef = db.collection("credentials").doc("client_secret")
+    var docRef = db.collection('credentials').doc('client_secret')
 
     var data = await docRef.get().then(function (doc) {
         if (doc.exists) {
             return doc.data()
         }
     }).catch(function (error) {
-        console.log("Error occurred getting secret. Trying again...")
+        console.log('Error occurred getting secret. Trying again...')
         return getClientSecret()
     })
 
@@ -16,7 +14,7 @@ async function getClientSecret() {
 }
 
 function generateUsername() {
-    return "temporaryUsername"
+    return 'temporaryUsername'
 }
 
 const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
@@ -24,9 +22,9 @@ const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay))
 function pingBridge(ip) {
     var username = generateUsername();
     $.ajax({
-        url: "https://" + ip + "/api",
-        type: "POST",
-        data: JSON.stringify({ "devicetype": username }),
+        url: 'https://' + ip + '/api',
+        type: 'POST',
+        data: JSON.stringify({ 'devicetype': username }),
         success: function (data) {
             console.log(data);
             if (data[0].error != null) {
@@ -37,7 +35,7 @@ function pingBridge(ip) {
             }
         },
         error: function (data) {
-            console.log("error!")
+            console.log('error!')
             console.log(data)
         }
     })
@@ -45,7 +43,7 @@ function pingBridge(ip) {
 
 async function connectToBridge(ip) {
     var connectedToBridge = false;
-    var bridgeUsername = ""
+    var bridgeUsername = ''
 
     while (!connectedToBridge) {
         var response = pingBridge(ip)
@@ -63,84 +61,112 @@ async function connectToBridge(ip) {
 }
 
 function spotifyLogin() {
-    location.replace("https://accounts.spotify.com/authorize?response_type=code&client_id=" + client_id + "&redirect_uri=" + redirect_uri + "&scope=user-read-currently-playing");
+    location.replace('https://accounts.spotify.com/authorize?response_type=code&client_id=' + client_id + '&redirect_uri=' + redirect_uri + '&scope=user-read-currently-playing');
 }
 
-function getToken() {
-    getClientSecret().then(function (value) {
-        var responseQuery = window.location.search
-        var re = /[&?]code=([^&]*)/
-        var code = re.exec(responseQuery)[1]
-        var client_secret = value
-
+async function getToken(clientSecret) {
+    var responseQuery = window.location.search
+    var re = /[&?]code=([^&]*)/
+    var code = re.exec(responseQuery)[1]
+    const result = new Promise(function (resolve, reject) {
         $.ajax({
-            type: "POST",
+            type: 'POST',
             url: 'https://accounts.spotify.com/api/token',
             data: {
-                "grant_type": 'authorization_code',
-                "code": code,
-                "redirect_uri": redirect_uri,
+                'grant_type': 'authorization_code',
+                'code': code,
+                'redirect_uri': redirect_uri,
             },
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
-                'Authorization': 'Basic ' + btoa(client_id + ':' + client_secret)
+                'Authorization': 'Basic ' + btoa(client_id + ':' + clientSecret)
             },
             success: function (data) {
+                console.log('token retrieved')
                 console.log(data)
-                return data.access_token;
+                var accessToken = data.access_token;
+                resolve(accessToken)
             },
             error: function (data) {
-                console.log("error!")
+                console.log('token error')
                 console.log(data)
+                reject("error")
             }
         })
     })
+
+    return result
 }
 
-function getCurrentSong() {
-    getToken().then(function(value) {
-        var accessToken = value
-    
-        $.ajax({
-            type: "GET",
-            url: "https://api.spotify.com/v1/me/player/currently-playing",
-            headers: {
-                "Accept": "application/json",
-                "Content-Type": "application/json",
-                "Authorization": "Bearer "+ accessToken
-            }, success: function(data) {
-                console.log(data)
-            }, error: function(data) {
-                console.log("Error!")
-                console.log(data)
-            }
-        })
+async function getRefreshToken(clientSecret, refreshToken) {
+    $.ajax({
+        type: 'POST',
+        url: 'https://accounts.spotify.com/api/token',
+        data: {
+            'grant_type': 'refresh_token',
+            'refresh_token': refreshToken,
+        },
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic ' + btoa(client_id + ':' + clientSecret)
+        },
+        success: function (data) {
+            console.log('token retrieved')
+            console.log(data)
+            return data.access_token;
+        },
+        error: function (data) {
+            console.log('token error')
+            console.log(data)
+        }
     })
 }
 
-function main() {
-    //var ip = prompt("enter ip:");
+async function getCurrentSong() {
+    var accessToken = await getToken()
+    console.log(accessToken + ' is token')
+
+    $.ajax({
+        type: 'GET',
+        url: 'https://api.spotify.com/v1/me/player/currently-playing',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + accessToken
+        }, success: function (data) {
+            console.log(data)
+        }, error: function (data) {
+            console.log('Error!')
+            console.log(data)
+        }
+    })
+}
+
+async function main() {
+    //var ip = prompt('enter ip:');
     //connectToBridge(ip)
     //getCurrentSong()
-    console.log("updated 2");
 
     firebase.initializeApp({
-        apiKey: "AIzaSyCoWUDx03Onb9JDj2MOqvjiTUzHAVrwzyY",
-        authDomain: "spotify-hue.firebaseapp.com",
-        projectId: "spotify-hue",
-        storageBucket: "spotify-hue.appspot.com",
-        messagingSenderId: "18502599113",
-        appId: "1:18502599113:web:c7627cea0aa74a3efd6818",
-        measurementId: "G-XZ8PE0K3Y1"
+        apiKey: 'AIzaSyCoWUDx03Onb9JDj2MOqvjiTUzHAVrwzyY',
+        authDomain: 'spotify-hue.firebaseapp.com',
+        projectId: 'spotify-hue',
+        storageBucket: 'spotify-hue.appspot.com',
+        messagingSenderId: '18502599113',
+        appId: '1:18502599113:web:c7627cea0aa74a3efd6818',
+        measurementId: 'G-XZ8PE0K3Y1'
     });
 
     db = firebase.firestore()
 
-    let client_secret;
 
-    getClientSecret().then(function (value) {
-        client_secret = value
+    getClientSecret().then(async function (clientSecret) {
+            
+        token = await getToken(clientSecret)
+        console.log(token)
     })
+
+
 
 }
 
