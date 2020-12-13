c4b99c2810b5808b5f5f5f1ed32864e0ce7fccfa
Devan <devankav7@gmail.com>
2020-12-13 18:39:12 -0500

    fixed redirect_uri

--- assets/scripts/credentials.js
+++ assets/scripts/credentials.js
@@ -1,3 +1,3 @@
 var client_id = "fc2304e3d3cb492b80b3c7bcb900f98a";
 var redirect_uri = "https://devanturtle7.github.io/spotify-hue/"
-redirect_uri="http://127.0.0.1:5500/"
+//redirect_uri="http://127.0.0.1:5500/"
